be4b6c1d94a68b296a69b930c90a463be69fc798
vathripada-a11y <vathripada@gmail.com>
2026-03-29 10:28:58 +0530

    Fix port to 7860 for HF Spaces

--- server/app.py
+++ server/app.py
@@ -23,7 +23,7 @@ app = create_app(
 )
 
 
-def main(host: str = "0.0.0.0", port: int = 8000):
+def main(host: str = "0.0.0.0", port: int = 7860):
     import uvicorn
     uvicorn.run(app, host=host, port=port)
 
